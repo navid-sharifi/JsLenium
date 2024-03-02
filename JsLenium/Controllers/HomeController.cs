93232cefa164845cd65645bb13fe6298ef34973d
Navid <navid0523@gmail.com>
2024-03-02 16:41:47 +0330

    Add project files.

---
+++ JsLenium/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JsLenium.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+
+        [Route("initial")]
+        public IActionResult Initial()
+        {
+            return View();
+        }
+
+        [Route("GetCommand")]
+        public IActionResult GetCommand()
+        {
+            return Ok("Command");
+        }
+
+    }
+}
